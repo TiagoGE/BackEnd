8df90cae5204674cf614f8b5759fb6fdc7610e5f
Tiago Guerra Endsfeldz <tiago.guerrae@gmail.com>
2024-10-20 16:37:35 -0700

    fixed a few bugs in middlewares and routes

--- API/src/routes/patient.ts
+++ API/src/routes/patient.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { createPatient, deletetePatient, getPatients, updatePatient } from "../Controllers/patients";
+import { VerifyToken } from "../Middlewares/verifyToken";
 
 const router = Router();
 
 
-router.get('/', getPatients);
-router.post('/', createPatient);
-router.put('/:id', updatePatient);
-router.delete('/:id', deletetePatient);
+router.get('/', VerifyToken, getPatients);
+router.post('/', VerifyToken, createPatient);
+router.put('/:id', VerifyToken, updatePatient);
+router.delete('/:id', VerifyToken, deletetePatient);
 
 
 export default router;
