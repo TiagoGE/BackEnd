38f8387e544dad7c4cd8f143f7b3e07b4555b3dc
Tiago Guerra Endsfeldz <tiago.guerrae@gmail.com>
2024-12-12 16:34:39 -0800

    Changed start script

--- API/src/routes/diagnosis.ts
+++ API/src/routes/diagnosis.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { createDiagnosis, getDiagnosisForUser, updateDiagnosis } from "../Controllers/diagnosis";
+import { VerifyToken } from "../Middlewares/verifyToken";
 
 const router = Router();
 
-router.post('/', createDiagnosis);
-router.put('/:id', updateDiagnosis);
-router.post('/user', getDiagnosisForUser);
+router.post('/', VerifyToken, createDiagnosis);
+router.put('/:id', VerifyToken, updateDiagnosis);
+router.post('/user', VerifyToken, getDiagnosisForUser);
 
 export default router;
